b0d99ed595ee33e52554a09260547e42925f777e
Miguel Monzón <mmonzonjr@gmail.com>
2023-03-10 00:34:57 -0600

    Tarea 5 y 6

--- ex4/api/students.js
+++ ex4/api/students.js
@@ -5,10 +5,11 @@ const router = express.Router();
 const { faker } = require('@faker-js/faker');
 
 const DEFAULT_LIMIT = 20;
-const students = [];
+//const students = [];
 
 router.post('/account', (req, res) => {
     const limit = req.query.limit || DEFAULT_LIMIT;
+    const students = [];
 
     for (let i = 0; i < limit; i++) {
         let name = faker.name.firstName();
@@ -30,12 +31,139 @@ router.post('/account', (req, res) => {
     res.send(students);
 });
 
+/*
 router.get('/:id', (req, res) => {
     const { id } = req.params;
     const student = students.find((student) => student.id === id);
     res.send({student});
 });
+*/
 
+/**
+ * 
+ *Get students with scholarship
+ *
+ */
+router.get('/scholarshipHolder', (req, res) => {
+    const limit = req.query.limit || DEFAULT_LIMIT;
+    const students = [];
+    const response = [];
+
+    for (let i = 0; i < limit; i++) {
+        let name = faker.name.firstName();
+        let lastName = faker.name.lastName();
+        const student = {
+            id: `0${i + 1}`,
+            name: name,
+            lastName: lastName,
+            age: getRandomIntInclusive(18, 28),
+            grade: getRandomIntInclusive(1, 9),
+            email: faker.internet.email(name, lastName),
+            is_regular: getRandomBoolRegular(),
+            is_ScholarshipHolder: getRandomBoolSchoolarship()
+        };
+    
+        students.push(student);
+    }
+
+    for(let i = 0; i < students.length; i++) {
+        if(students[i].is_ScholarshipHolder === true) {
+            response.push(students[i]);
+        }
+    }
+
+    res.send({
+        message: 'Students:',
+        all: students,
+        scholarshipStudents: response
+    });
+});
+
+/**
+ * 
+ *Get legal age students
+ *
+ */
+router.get('/legalAge', (req, res) => {
+    const limit = req.query.limit || DEFAULT_LIMIT;
+    const students = [];
+    const response = [];
+
+    for (let i = 0; i < limit; i++) {
+        let name = faker.name.firstName();
+        let lastName = faker.name.lastName();
+        const student = {
+            id: `0${i + 1}`,
+            name: name,
+            lastName: lastName,
+            age: getRandomIntInclusive(18, 28),
+            grade: getRandomIntInclusive(1, 9),
+            email: faker.internet.email(name, lastName),
+            is_regular: getRandomBoolRegular(),
+            is_ScholarshipHolder: getRandomBoolSchoolarship()
+        };
+    
+        students.push(student);
+    }
+
+    for(let i = 0; i < students.length; i++) {
+        if(students[i].age >= 21) {
+            response.push(students[i]);
+        }
+    }
+
+    res.send({
+        message: 'Students:',
+        all: students,
+        legalAgeStudents: response
+    });
+});
+
+/**
+ * 
+ *Get students by range age (26 - 28)
+ *
+ */
+router.get('/rangeAge', (req, res) => {
+    const limit = req.query.limit || DEFAULT_LIMIT;
+    const students = [];
+    const response = [];
+
+    for (let i = 0; i < limit; i++) {
+        let name = faker.name.firstName();
+        let lastName = faker.name.lastName();
+        const student = {
+            id: `0${i + 1}`,
+            name: name,
+            lastName: lastName,
+            age: getRandomIntInclusive(18, 28),
+            grade: getRandomIntInclusive(1, 9),
+            email: faker.internet.email(name, lastName),
+            is_regular: getRandomBoolRegular(),
+            is_ScholarshipHolder: getRandomBoolSchoolarship()
+        };
+    
+        students.push(student);
+    }
+
+    for(let i = 0; i < students.length; i++) {
+        if(students[i].age >= 26 && students[i].age <= 28) {
+            response.push(students[i]);
+        }
+    }
+
+    res.send({
+        message: 'Students:',
+        all: students,
+        rangeAgeStudents: response
+    });
+});
+
+/**
+ * 
+ *Funtions
+ *
+ */
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
--- ex4/api/teachers.js
+++ ex4/api/teachers.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const { faker, RandomModule } = require('@faker-js/faker');
+const { response } = require('express');
 
 const DEFAULT_LIMIT = 20;
-const teachers = [];
+//const teachers = [];
 
 router.post('/account', (req, res) => {
     const limit = req.query.limit || DEFAULT_LIMIT;
+    const teachers = [];
     
     for (let i = 0; i < limit; i++) {
         let gender = getRandomGender();
@@ -30,12 +32,180 @@ router.post('/account', (req, res) => {
     res.send(teachers);
 });
 
+/*
 router.get('/:id', (req, res) => {
     const { id } = req.params;
     const teacher = teachers.find((teacher) => teacher.id === id);
     res.send({teacher});
 });
+*/
 
+/**
+ * 
+ *Get 'X' teachers 
+ *
+ */
+ router.get('/teacher/xTeachers/:number', (req, res) => {
+    const limit = req.query.limit || DEFAULT_LIMIT;
+    const teachers = [];
+    const response = [];
+    
+    for (let i = 0; i < limit; i++) {
+        let gender = getRandomGender();
+        let name = faker.name.firstName(gender);
+        let lastName = faker.name.lastName();
+        const teacher = {
+            id: `0${i + 1}`,
+            name: name,
+            lastName: lastName,
+            age: getRandomIntInclusive(26, 99),
+            gender: gender,
+            email: faker.internet.email(name, lastName),
+            movil: faker.phone.number('55-####-####'),
+            address: faker.address.streetAddress()
+        };
+
+        teachers.push(teacher);
+    }
+    const { number } = req.params;
+    for(let i = 0; i < teachers.length; i++) {
+        while(i != number) {
+            response.push(teachers[i])
+        }
+    }
+
+    res.send ({
+        message: 'Teachers:',
+        all: teachers,
+        xTeachers: response
+    });
+});
+
+/**
+ * 
+ *Get teachers by id
+ *
+ */
+router.get('/teacher/:id', (req, res) => {
+    const limit = req.query.limit || DEFAULT_LIMIT;
+    const teachers = [];
+    
+    for (let i = 0; i < limit; i++) {
+        let gender = getRandomGender();
+        let name = faker.name.firstName(gender);
+        let lastName = faker.name.lastName();
+        const teacher = {
+            id: `0${i + 1}`,
+            name: name,
+            lastName: lastName,
+            age: getRandomIntInclusive(26, 99),
+            gender: gender,
+            email: faker.internet.email(name, lastName),
+            movil: faker.phone.number('55-####-####'),
+            address: faker.address.streetAddress()
+        };
+
+        teachers.push(teacher);
+    }
+
+    const { id } = req.params;
+    const teacher = teachers.find((teacher) => teacher.id === id);
+    
+    res.send ({
+        message: 'Teachers:',
+        all: teachers,
+        teacherById: teacher
+    });
+});
+
+/**
+ * 
+ *Get teachers group by first letter on his last name
+ *
+ */
+router.get('/teacher/firstLetterLastName/:letter', (req, res) => {
+    const limit = req.query.limit || DEFAULT_LIMIT;
+    const teachers = [];
+    const response = [];
+    
+    for (let i = 0; i < limit; i++) {
+        let gender = getRandomGender();
+        let name = faker.name.firstName(gender);
+        let lastName = faker.name.lastName();
+        const teacher = {
+            id: `0${i + 1}`,
+            name: name,
+            lastName: lastName,
+            age: getRandomIntInclusive(26, 99),
+            gender: gender,
+            email: faker.internet.email(name, lastName),
+            movil: faker.phone.number('55-####-####'),
+            address: faker.address.streetAddress()
+        };
+
+        teachers.push(teacher);
+    }
+
+    const { letter } = req.params;
+    for(let i = 0; i < teachers.length; i++) {
+        if(letter === teachers[i].lastName.charAt(0)) {
+            response.push(teachers[i]);
+        }
+    }
+
+    res.send({
+        message: 'Teachers:',
+        all: teachers,
+        teachersByLastName: response
+    });
+});
+
+/**
+ * 
+ *Get teacher by range age (26 - 28)
+ *
+ */
+router.get('/rangeAge', (req, res) => {
+    const limit = req.query.limit || DEFAULT_LIMIT;
+    const teachers = [];
+    const response = [];
+    
+    for (let i = 0; i < limit; i++) {
+        let gender = getRandomGender();
+        let name = faker.name.firstName(gender);
+        let lastName = faker.name.lastName();
+        const teacher = {
+            id: `0${i + 1}`,
+            name: name,
+            lastName: lastName,
+            age: getRandomIntInclusive(26, 99),
+            gender: gender,
+            email: faker.internet.email(name, lastName),
+            movil: faker.phone.number('55-####-####'),
+            address: faker.address.streetAddress()
+        };
+
+        teachers.push(teacher);
+    }
+
+    for(let i = 0; i < teachers.length; i++) {
+        if(teachers[i].age >= 26 && teachers[i].age <= 28) {
+            response.push(teachers[i]);
+        }
+    }
+
+    res.send({
+        message: 'Teachers:',
+        all: teachers,
+        rangeAgeTeachers: response
+    });
+});
+
+/**
+ * 
+ *Funtions
+ *
+ */
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
