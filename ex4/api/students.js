524c835273568ad1f3cf3a3fb57a719a1b460cb8
Miguel Monzón <mmonzonjr@gmail.com>
2023-03-09 19:50:16 -0600

    Tarea 5 y 6

--- ex4/api/students.js
+++ ex4/api/students.js
@@ -5,34 +5,61 @@ const router = express.Router();
 const { faker } = require('@faker-js/faker');
 
 const DEFAULT_LIMIT = 20;
+const students = [];
 
-router.get('/', (req, res) => {
+router.post('/account', (req, res) => {
     const limit = req.query.limit || DEFAULT_LIMIT;
 
-    const students = [];
-
     for (let i = 0; i < limit; i++) {
+        let name = faker.name.firstName();
+        let lastName = faker.name.lastName();
         const student = {
-            id: `0000${i + 1}`,
-            name: faker.name.firstName(),
-            lastName: faker.name.lastName()
-            // age, grade: use the random inclusive function
-            // email: faker
-            // is_regular: use Math.random() true 60% probabilities
-            // is_regular: use Math.random() true 20% probabilities
-    };
+            id: `0${i + 1}`,
+            name: name,
+            lastName: lastName,
+            age: getRandomIntInclusive(18, 28),
+            grade: getRandomIntInclusive(1, 9),
+            email: faker.internet.email(name, lastName),
+            is_regular: getRandomBoolRegular(),
+            is_ScholarshipHolder: getRandomBoolSchoolarship()
+        };
     
-    students.push(student);
-  }
+        students.push(student);
+    }
 
-  res.send(students);
+    res.send(students);
 });
 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
-    res.send({
-        message: `searching the id: ${id}`
-    });
+    const student = students.find((student) => student.id === id);
+    res.send({student});
 });
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomBoolRegular() {
+    prob = getRandomIntInclusive(1, 100);
+    if(prob >= 0 && prob <= 60) {
+        return true;
+    }
+    else if(prob >= 61 && prob <= 100){
+        return false;
+    }
+}
+
+function getRandomBoolSchoolarship() {
+    prob = getRandomIntInclusive(1, 100);
+    if(prob >= 0 && prob <= 20) {
+        return true;
+    }
+    else if(prob >= 21 && prob <= 100){
+        return false;
+    }
+}
 
 module.exports = router;
