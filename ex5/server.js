d1b943458788baea4806b554c9550a2b1218bdda
Miguel Monzón <mmonzonjr@gmail.com>
2023-03-15 08:29:11 -0600

    Clase 15/03/23

---
+++ ex5/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const app = require('./app');
+const dbConnection = require('./db/mongodb');
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+dbConnection(MONGO_URI)
+    .then(() => {
+        console.log('db Connected');
+    })
+    .catch(err => console.log(err));
+
+app.listen
