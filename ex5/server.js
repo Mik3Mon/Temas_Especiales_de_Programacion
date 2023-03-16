f8326e6924f5278a0f46ed728348473e72ff8f7d
Miguel Monzón <mmonzonjr@gmail.com>
2023-03-15 21:05:36 -0600

    Clase 15/03/23

--- ex5/server.js
+++ ex5/server.js
@@ -7,8 +7,10 @@ const MONGO_URI = process.env.MONGO_URI;
 
 dbConnection(MONGO_URI)
     .then(() => {
-        console.log('db Connected');
+        console.log(`db connected`);
     })
     .catch(err => console.log(err));
 
-app.listen
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
