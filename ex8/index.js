a9ee57ee16cc239eb8599648c9b555b2f82071bc
Miguel Monzón <mmonzonjr@gmail.com>
2023-04-25 23:05:52 -0600

    Clase 24/04/23

--- ex8/index.js
+++ ex8/index.js
@@ -5,9 +5,13 @@
  */
 
 const twoSum = function(nums, target) {
-
     for (let i = 0; i < nums.length - 1; i++) {
-        
+        for (let j = i + 1; j < nums.length; j++) {
+            const sum = nums[i] + nums[j];
+            if (sum === target) {
+                return [i, j];
+            }
+        }
     }
 };
 
